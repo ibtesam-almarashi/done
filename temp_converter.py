28866891a20740e27afd5a9a6fbd02e5877c8d01
ibtesam <almarashi1411@gmail.com>
2025-12-03 20:23:11 +0300

    Add temperature converter in Python

---
+++ temp_converter.py
@@ -0,0 +1,4 @@
+tempeC = input("Enter the Celsius temperature you want to convert to Fahrenheit\n")
+tempeC = float(tempeC)
+tempF = tempeC * 1.8 + 32
+print(f"The temperature in Fahrenheit is:\n{tempF}")
